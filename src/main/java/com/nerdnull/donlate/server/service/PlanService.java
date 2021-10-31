b0b90a586a39a6712c2b97b94616556df5f1d72c
kimyoungwoong <kyw04004@naver.com>
2021-10-31 14:49:52 +0900

    reflected review

--- src/main/java/com/nerdnull/donlate/server/service/PlanService.java
+++ src/main/java/com/nerdnull/donlate/server/service/PlanService.java
@@ -5,21 +5,20 @@ import com.nerdnull.donlate.server.mapper.PlanStateMapper;
 import com.nerdnull.donlate.server.domain.PlanEntity;
 import com.nerdnull.donlate.server.dto.PlanDto;
 import com.nerdnull.donlate.server.repository.PlanRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.factory.Mappers;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class PlanService {
 
     private final PlanRepository planRepository;
     private final PlanMapper planMapper = Mappers.getMapper(PlanMapper.class);
     private final PlanStateMapper planStateMapper = Mappers.getMapper(PlanStateMapper.class);
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public PlanService(PlanRepository planRepository) {
@@ -29,8 +28,8 @@ public class PlanService {
     public PlanDto getDetails(Long planId) {
         Optional<PlanEntity> maybePlan=  planRepository.findById(planId);
         if(maybePlan.isEmpty()) {
-            logger.error("[ERROR] PlanService/getDetails -> Not exists plan");
-            throw new IllegalArgumentException("PlanService/getDetails -> Not exists plan");
+            log.error("Not exists plan");
+            throw new IllegalArgumentException("Not exists plan");
         }
         PlanDto plan = planMapper.toDto(maybePlan.get());
         plan.setPlanStateList(planStateMapper.toDtoList(maybePlan.get().getPlanStateList()));
