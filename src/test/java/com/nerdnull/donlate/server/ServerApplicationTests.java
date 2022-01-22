6ee8a216a203096d6fc6dd28883fca1652175add
kimyoungwoong <kyw04004@naver.com>
2022-01-22 15:25:53 +0900

    resolve conflicts3

--- src/test/java/com/nerdnull/donlate/server/ServerApplicationTests.java
+++ src/test/java/com/nerdnull/donlate/server/ServerApplicationTests.java
@@ -1,7 +1,6 @@
 package com.nerdnull.donlate.server;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
